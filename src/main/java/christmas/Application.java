51d614ad35bdf473c11487b0eb765893793da21a
wooni97 <jhw970714@gmail.com>
2023-11-12 15:52:25 +0900

    docs: 구현 기능 목록 수정

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,11 @@
 package christmas;
 
+import christmas.controller.PromotionController;
+import christmas.view.InputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        PromotionController promotionController = new PromotionController(new InputView());
+        promotionController.run();
     }
 }
