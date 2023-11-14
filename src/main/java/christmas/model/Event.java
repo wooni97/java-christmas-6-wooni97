f003f3bfe11fe1cb0e3bbba3794ee5a7670e2d6f
wooni97 <jhw970714@gmail.com>
2023-11-14 20:12:54 +0900

    refactor: Event 추상 클래스 Interface로 변경
    - 이 커밋은 기존의 Event 추상 클래스를 Interface로 변경합니다. 하위 클래스 중 불필요한 의존성을 가진 경우가 있어 인터페이스를 사용하여 코드의 유연성과 확장성을 높입니다.

--- src/main/java/christmas/model/Event.java
+++ src/main/java/christmas/model/Event.java
@@ -1,23 +1,7 @@
 package christmas.model;
 
-import java.time.LocalDate;
-
-public abstract class Event {
-
-    protected final LocalDate orderDate;
-    protected final Orders orders;
-
-    public Event(LocalDate orderDate, Orders orders) {
-        this.orderDate = orderDate;
-        this.orders = orders;
-    }
-
-    public abstract int calculateDiscount();
-
-    /*
-    * 이벤트 적용 대상 여부
-    * */
-    public abstract boolean isEventActive();
-
-    public abstract String getEventName();
+public interface Event {
+    int calculateDiscount();
+    boolean isEventActive();
+    String getEventName();
 }
