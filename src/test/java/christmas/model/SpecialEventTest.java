b494e492a0f917aa44be1988dd0b4da701daa89f
wooni97 <jhw970714@gmail.com>
2023-11-15 21:56:45 +0900

    test: SpecialEvent 도메인 테스트 코드 수정

--- src/test/java/christmas/model/SpecialEventTest.java
+++ src/test/java/christmas/model/SpecialEventTest.java
@@ -5,13 +5,13 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
-import java.util.Arrays;
 
 class SpecialEventTest {
 
-    @DisplayName("")
+    @DisplayName("특별 할인 날이면 할인 금액이 1000원과 일치한다")
     @Test
     void specialDayEventDiscountCalculateTest() {
+
         //given
         LocalDate orderDate = LocalDate.of(2023,12,3);
 
@@ -22,9 +22,10 @@ class SpecialEventTest {
         assertEquals(1000, specialEvent.calculateDiscount());
     }
 
-    @DisplayName("")
+    @DisplayName("특별 할인 날이 아니면 할인 금액이 0원과 일치한다")
     @Test
     void notSpecialDayDiscountCalculateTest() {
+
         //given
         LocalDate orderDate = LocalDate.of(2023,12,11);
 
@@ -33,6 +34,34 @@ class SpecialEventTest {
 
         //then
         assertEquals(0, specialEvent.calculateDiscount());
+    }
+
+    @DisplayName("특별 할인 날이면 이벤트 적용 true")
+    @Test
+    void specialDayEventActiveTest() {
+
+        //given
+        LocalDate orderDate = LocalDate.of(2023,12,3);
+
+        //when
+        SpecialEvent specialEvent = new SpecialEvent(orderDate);
+
+        //then
+        assertTrue(specialEvent.isEventActive());
+    }
+
+    @DisplayName("특별 할인 날이 아니면 이벤트 적용 false")
+    @Test
+    void notSpecialDayEventActiveTest() {
+
+        //given
+        LocalDate orderDate = LocalDate.of(2023,12,4);
+
+        //when
+        SpecialEvent specialEvent = new SpecialEvent(orderDate);
+
+        //then
+        assertFalse(specialEvent.isEventActive());
     }
 
 }
