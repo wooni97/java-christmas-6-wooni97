b8a5d04409071b2ce4aab6322b5186842a964604
wooni97 <jhw970714@gmail.com>
2023-11-15 20:00:11 +0900

    feat: Dto 클래스 OrderDtos 구현
    - 이 커밋은 입/출력 레이어에서 주문 메뉴와 개수를 수월하게 다루고자 dto 역할을 위한 OrderDtos 클래스를 추가합니다.

---
+++ src/main/java/christmas/dto/OrderDtos.java
@@ -0,0 +1,39 @@
+package christmas.dto;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class OrderDtos implements Iterable<OrderDtos.OrderDto> {
+
+    private final List<OrderDto> orderDtos;
+
+    public OrderDtos(List<OrderDto> orderDtos) {
+        this.orderDtos = new ArrayList<>(orderDtos);
+    }
+
+    @Override
+    public Iterator<OrderDto> iterator() {
+        return orderDtos.iterator();
+    }
+
+
+    public static class OrderDto {
+        private final String menuName;
+        private final int orderCount;
+
+        public OrderDto(String menuName, int orderCount) {
+            this.menuName = menuName;
+            this.orderCount = orderCount;
+        }
+
+        public String getMenuName() {
+            return menuName;
+        }
+
+        public int getOrderCount() {
+            return orderCount;
+        }
+    }
+
+}
