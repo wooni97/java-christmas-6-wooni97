5a496519a23f0703a1646a1380e9482485eaaf68
wooni97 <jhw970714@gmail.com>
2023-11-15 20:08:26 +0900

    refactor: 메인 메서드 수정
    - 이 커밋은 컨트롤러의 의존성 변경에 따라 메인 메서드를 수정합니다.

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,11 +1,13 @@
 package christmas;
 
-import christmas.controller.PromotionController;
+import christmas.controller.EventPlannerController;
+import christmas.service.EventPlannerService;
 import christmas.view.InputView;
+import christmas.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        PromotionController promotionController = new PromotionController(new InputView());
-        promotionController.run();
+        EventPlannerController eventPlannerController = new EventPlannerController(new InputView(), new OutputView(), new EventPlannerService());
+        eventPlannerController.run();
     }
 }
